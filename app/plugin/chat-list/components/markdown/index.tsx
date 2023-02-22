f87ce8659277cb75bad43b66ffc96561727a57b4
Francis <francisshi@mindverse.ai>
2023-02-22 15:37:40 +0800

    // 修复markdown html中，p标签自动加边距的情况

--- app/plugin/chat-list/components/markdown/index.tsx
+++ app/plugin/chat-list/components/markdown/index.tsx
@@ -13,10 +13,23 @@ export default function Markdown(props: MarkdownInterface) {
     const converter = new showdown.Converter();
     const html = converter.makeHtml(content);
     setMarkdown(html);
+    // 修复markdown html中，p标签自动加边距的情况
+    setTimeout(() => {
+      const pList = document.getElementsByTagName('p');
+      if (pList && pList.length > 0) {
+        for (let i = 0; i < pList.length; i++) {
+          const ele = pList[i];
+          if (ele.parentElement?.id === 'mv_markdown') {
+            ele.style.margin = '0px';
+          }
+        }
+      }
+    }, 0);
   }, [content]);
 
   return (
     <div
+      id={`mv_markdown`}
       contentEditable="true"
       dangerouslySetInnerHTML={{__html: markdown}}
     ></div>
