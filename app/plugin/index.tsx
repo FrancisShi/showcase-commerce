143b7b3337c8b79f2dad5960970e00c0a9accdcc
Francis <francisshi@mindverse.ai>
2023-02-24 15:28:42 +0800

    update

--- app/plugin/index.tsx
+++ app/plugin/index.tsx
@@ -265,6 +265,7 @@ function App(props: {
           visibility: !isExpand ? 'hidden' : 'visible',
         }}
       >
+        {/* 返回框 */}
         <div
           style={{
             position: 'absolute',
