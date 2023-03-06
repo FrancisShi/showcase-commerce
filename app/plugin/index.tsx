70af151b269709e6150e37d44a0a358b24742b68
Francis <francisshi@mindverse.ai>
2023-03-06 20:46:56 +0800

    grant 判断

--- app/plugin/index.tsx
+++ app/plugin/index.tsx
@@ -317,29 +317,46 @@ function App(props: {
 
   // 长按录音
   useEffect(() => {
+    let granted = false;
     let recorder: Recorder;
 
     const recorderImg = document.getElementById('voiceRecorder');
     const eventAreaStart = (e: TouchEvent | MouseEvent) => {
-      recorder = new Recorder({
-        sampleBits: 16, // 采样位数，支持 8 或 16，默认是16
-        sampleRate: 16000, // 采样率，支持 11025、16000、22050、24000、44100、48000，根据浏览器默认值，我的chrome是48000
-        numChannels: 1,
-      });
-      recorder.start().then(
-        () => {
-          setIsRecording(true);
-          // 开始录音
-          alert('test1');
-          console.log('开始录音了=========');
-        },
-        (error) => {
-          alert('test2');
-          setIsRecording(false);
-          // 出错了
-          console.log(error);
-        },
-      );
+      if (navigator.mediaDevices.getUserMedia) {
+        if (!granted) {
+          const constraints = {audio: true};
+          navigator.mediaDevices.getUserMedia(constraints).then(
+            (stream) => {
+              console.log('授权成功！');
+              granted = true;
+            },
+            () => {
+              console.error('授权失败！');
+            },
+          );
+        } else {
+          recorder = new Recorder({
+            sampleBits: 16, // 采样位数，支持 8 或 16，默认是16
+            sampleRate: 16000, // 采样率，支持 11025、16000、22050、24000、44100、48000，根据浏览器默认值，我的chrome是48000
+            numChannels: 1,
+          });
+
+          recorder.start().then(
+            () => {
+              setIsRecording(true);
+              // 开始录音
+              console.log('开始录音了=========');
+            },
+            (error) => {
+              setIsRecording(false);
+              // 出错了
+              console.error(error);
+            },
+          );
+        }
+      } else {
+        alert('浏览器不支持 getUserMedia');
+      }
     };
     const eventAreaFinish = (e: TouchEvent | MouseEvent) => {
       setIsRecording(false);
