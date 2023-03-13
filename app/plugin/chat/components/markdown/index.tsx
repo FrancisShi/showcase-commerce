b03a14dec5e3a5bf54211dbe3638fcf033c885c2
Francis <francisshi@mindverse.ai>
2023-03-13 15:13:52 +0800

    update

--- app/plugin/chat/components/markdown/index.tsx
+++ app/plugin/chat/components/markdown/index.tsx
@@ -1,26 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import showdown from 'showdown';
-
-const temp = `<div style="position: relative; width: 100%; height: 100px; border-radius:2px; opacity: 1; background: white; display:flex; flex-direction:row;">
-<img
-  style="object-fit: cover; width: 86px; height: 86px; margin-left: 8px; margin-top: 7px;"
-  src="https://cdn.shopify.com/s/files/1/0724/4188/9082/products/screenshot-20230222-170516.png?v=1677056793"
-  alt=""
-/>
-  <div onclick="(function(){
-    var e = new Event('mv_client_container_router');
-    e.detail='https://showcase-commerce-049eb715bd4aa0080860.o2.myshopify.dev/products/vevelstad-bed-frame-white-180x200-cm';
-    window.dispatchEvent(e);
-  })();">
-    <div style="position: absolute;line-height:20px; height:40px; overflow:hidden; top:14px; left: 110px; font-size: 12px; color: #3D3D3D;">
-      VEVELSTAD Bed frame, white, 180x200 cm131231231231312hellohellohellohellohello
-    </div>
-  </div>
-  <div style="position: absolute; bottom:17px; left: 110px; font-size: 16px; font-weight: bold; color: #3D3D3D;">
-    1833.00
-  </div>
-</div>`;
-
+import {marked} from 'marked';
 export interface MarkdownInterface {
   content: string;
 }
@@ -30,8 +9,7 @@ export default function Markdown(props: MarkdownInterface) {
   const [markdown, setMarkdown] = useState<string>(content);
 
   useEffect(() => {
-    const converter = new showdown.Converter();
-    const html = converter.makeHtml(content);
+    const html = marked.parse(content);
     setMarkdown(html);
     // 修复markdown html中，p标签自动加边距的情况
     setTimeout(() => {
@@ -53,17 +31,23 @@ export default function Markdown(props: MarkdownInterface) {
         }
       }
 
+      const codes = document.getElementsByTagName('code');
+      if (codes && codes.length > 0) {
+        for (let i = 0; i < codes.length; i++) {
+          const ele = codes[i];
+          ele.style.setProperty('white-space', 'pre-wrap', 'important');
+        }
+      }
+
       const handleList = (list: HTMLCollectionOf<any>, style: string) => {
         if (list && list.length > 0) {
           for (let i = 0; i < list.length; i++) {
             const ele = list[i];
-            if (ele.parentElement?.id === 'mv_markdown') {
-              // 处理 ul
-              ele.style.listStyleType = style ?? 'disc';
-              ele.style.paddingLeft = '16px';
-              ele.style.marginTop = '16px';
-              ele.style.marginBottom = '16px';
-            }
+            // 处理 ul
+            ele.style.listStyleType = style ?? 'disc';
+            ele.style.paddingLeft = '16px';
+            ele.style.marginTop = '16px';
+            ele.style.marginBottom = '16px';
           }
         }
       };
